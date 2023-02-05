1ec8447b0c880f46a627ded5c81ac8c0fc8ffd3b
Mark Hollomon <mhhollomon@gmail.com>
2023-02-04 19:05:06 -0500

    cleanup scale list in menus

--- src/app/scale.service.ts
+++ src/app/scale.service.ts
@@ -12,25 +12,14 @@ const sonorityChoices : Choice<ScaleType>[] = [
 // Change with care.
 const sigChooser = new Chooser<number>([
   mkch(0, 20), 
-  mkch(1, 17), mkch(-1, 17),
-  mkch(2, 14), mkch(-2, 14),
-  mkch(3,  8), mkch(-3, 14),
-  mkch(4,  5), mkch(-4,  8),
+  mkch(1, 17), mkch( 2, 17),
+  mkch(3, 14), mkch( 4, 14),
+  mkch(5,  8), mkch( 6, 14),
+  mkch(7,  5), mkch( 8,  5),
+  mkch(9,  5), mkch(10,  8),
 ]);
 
-const sigOffset = 4;
-
-const sigNames = {
-  lydian     : [ 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C',  'G',  'D',  'A'  ],
-  major      : [ 'Ab', 'Eb', 'Bb', 'F',  'C', 'G',  'D',  'A',  'E'  ],
-  mixolydian : [ 'Eb', 'Bb', 'F',  'C',  'G', 'D',  'A',  'E',  'B'  ],
-  dorian     : [ 'Bb', 'F',  'C',  'G',  'D', 'A',  'E',  'B',  'F#' ],
-  minor      : [ 'F',  'C',  'G',  'D',  'A', 'E',  'B',  'F#', 'Db' ],
-  phrygian   : [ 'C',  'G',  'D',  'A',  'E', 'B',  'F#', 'Db', 'Ab' ],
-  locrian    : [ 'G',  'D',  'A',  'E',  'B', 'F#', 'Db', 'Ab', 'Eb' ],
-
-}
-
+const sigNames =  [ 'C', 'G', 'F', 'D', 'Bb', 'A', 'Eb', 'E', 'Ab', 'B', 'Db', 'F#', 'Gb' ];
 
 @Injectable({
   providedIn: 'root'
@@ -46,12 +35,11 @@ export class ScaleService {
     sonority = sonority ? sonority : this.sonorityChooser.choose();
 
     const keysig = sigChooser.choose();
-    const keycenter = sigNames[sonority][keysig + sigOffset];
+    const keycenter = sigNames[keysig];
     return new Scale({ center : keycenter, type : sonority });
   }
 
   getKeyList(sonority : ScaleType) : string[] {
-
-    return sigChooser.choices.map(v => sigNames[sonority][v.choice+sigOffset])
+    return sigChooser.choices.map(v => sigNames[v.choice])
   }
 }
